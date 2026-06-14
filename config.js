0bc790f1f3ffc3a777394e31638c93fd38684dd1
pkdy81 <aneunsuko91103@gmail.com>
2026-06-14 16:17:38 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // =============================================
 
 const SUPABASE_URL = 'https://your-project-id.supabase.co';
-const SUPABASE_ANON_KEY = 'your-anon-key-here';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpiZHRycmJibHBleGNqdm10dXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0MTAxMTQsImV4cCI6MjA5Njk4NjExNH0.kSc331N0non1eu3ofOoCoR0Tr1F66FJEkf6jXMfzulU';
